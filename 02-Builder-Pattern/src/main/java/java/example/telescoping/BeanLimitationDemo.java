48b2ccf5212a91659be044cb442d9a4092970723
Raj Bhatta <bhattaraj21@gmail.com>
2020-09-05 18:06:36 -0700

    Builder Pattern

---
+++ 02-Builder-Pattern/src/main/java/java/example/telescoping/BeanLimitationDemo.java
@@ -0,0 +1,20 @@
+package java.example.telescoping;
+
+public class BeanLimitationDemo {
+    private static void main(String [] args){
+
+        CustomerBean customerBean=new CustomerBean();
+
+        customerBean.setBread("Wheat");
+        customerBean.setCondiments("Lettuce");
+        customerBean.setDressing("Mustard");
+        customerBean.setMeat("Ham");
+
+
+        System.out.println(customerBean.getBread());
+        System.out.println(customerBean.getCondiments());
+        System.out.println(customerBean.getDressing());
+        System.out.println(customerBean.getMeat());
+
+    }
+}
---
+++ 02-Builder-Pattern/src/main/java/java/example/telescoping/CustomerBean.java
@@ -0,0 +1,43 @@
+package java.example.telescoping;
+
+public class CustomerBean {
+    private String bread;
+    private String condiments;
+    private String dressing;
+    private String meat;
+
+    public CustomerBean() {
+    }
+
+    public String getBread() {
+        return bread;
+    }
+
+    public void setBread(String bread) {
+        this.bread = bread;
+    }
+
+    public String getCondiments() {
+        return condiments;
+    }
+
+    public void setCondiments(String condiments) {
+        this.condiments = condiments;
+    }
+
+    public String getDressing() {
+        return dressing;
+    }
+
+    public void setDressing(String dressing) {
+        this.dressing = dressing;
+    }
+
+    public String getMeat() {
+        return meat;
+    }
+
+    public void setMeat(String meat) {
+        this.meat = meat;
+    }
+}
